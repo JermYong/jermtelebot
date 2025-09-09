33a0e1f38e8b41c4b279284257d91e9cae368128
JermYong <jeremyyong13@gmail.com>
2025-09-09 13:05:06 +0800

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import asyncio
+import logging
+from telebot import dp, bot  # Import your Dispatcher and Bot objects from telebot.py
+
+async def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
